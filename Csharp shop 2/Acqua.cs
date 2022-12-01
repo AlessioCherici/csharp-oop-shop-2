85d3373331df1b2d05aff0fce5c265c784ec261e
AlessioCherici <alessio.cherici@hotmail.it>
2022-12-01 17:08:22 +0100

    Creato primo oggetto con tutti i metodi

---
+++ Csharp shop 2/Acqua.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_shop_2
+    {
+    public class Acqua : Prodotto
+        {
+        private double litri;
+        private double pH;
+        private string sorgente;
+
+        public Acqua(string nome, string descrizione, double litri, double pH,
+            string sorgente, double prezzo, double iva) : base(nome, descrizione, prezzo, iva)
+            {
+            this.litri = litri;
+            this.pH = pH;   
+            this.sorgente = sorgente;
+            }
+
+        //--------------------------------- Metodi Get ---------------------------------
+
+        public double GetLitri()
+            {
+            return this.litri;
+            }
+        public double GetpH()
+            {
+            return this.pH;
+            }
+        public string GetSorgente()
+            {
+            return this.sorgente;
+            }
+        //--------------------------------- Metodi set ---------------------------------
+
+        public void SetLitri()
+            {
+            this.litri = litri;
+            }
+        public void SetpH()
+            {
+            this.pH = pH;
+            }
+        public void SetSorgente()
+            {
+            this.sorgente = sorgente;
+            }
+
+        public override void stampaTutto()
+            {
+            Console.WriteLine("---------" + base.getNome() + "---------" + "\n");
+            Console.WriteLine("Descrizione: " + base.getDescrizione());
+            Console.WriteLine("Capienza: " + this.litri + "L");
+            Console.WriteLine("pH: " + this.pH);
+            Console.WriteLine("Sorgente: " + this.sorgente);
+            Console.WriteLine("Codice Prodotto: " + base.getNumeroProdotto().ToString().PadLeft(8, '0'));
+            Console.WriteLine("Iva prodotto: " + base.getIva() + "%");
+            Console.WriteLine("Prezzo con Iva: " + base.prezzoIvato(base.getPrezzo(), base.getIva()) + "\n" );
+            Console.WriteLine("-----------------------------------");
+            }
+        }
+    }
--- Csharp shop 2/Program.cs
+++ Csharp shop 2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*A partire da quanto già fatto con csharp-oop-shop dove vi era stato chiesto di creare una classe Prodotto
+generica per gestire un generico prodotto dello Shop, vi chiedo ora di essere più specifichi e di creare 
+le sottoclassi opportune di alcuni prodotti che potremmo identificare nel nostro shop.
+In particolare immaginiamo che nel nostro shop possiamo commerciare le seguenti tipologie di prodotto:
+ * Acqua (massimo 1.5L)
+ * Sacchetto di Frutta a pezzi (al massimo puo contenere 5 pezzi di frutta)
+ * Elettrodomestico
+ * Cibo in Scatola (al massimo può contenere 300g di peso su una scatola)
+Per ognuno dei seguenti prodotti più specifici vi si chiede di identificare quali potrebbero essere
+i loro attributi e metodi che eventualmente gli riguardano, ereditando i metodi e gli attributi generici dalla classe Prodotto creata nell’esercizio precedente.*/
+
+using Csharp_shop_2;
+
+
+List<Prodotto> list = new List<Prodotto>();
+
+Acqua BottigliaAcqua = new Acqua("Bottiglia d'Acqua", "Una fin troppo comune bottiglia d'acqua naturale da 1.5L.", 1.5, 7, "Sorgente reale di Frosinone", 15, 22 );
+
+BottigliaAcqua.stampaTutto();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
